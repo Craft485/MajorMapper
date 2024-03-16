04cfb45ed77d107ce90147a8ae706b18b209e3f9
Colin Davis <colindavis5@gmail.com>
2024-03-16 16:52:15 -0400

    Add web prototype

---
+++ web/public/client.js
@@ -0,0 +1,125 @@
+// We are unable to do something simple use deconsutrction here because of some oddities with the way that DOM methods work
+if (!$) var $ = document.querySelectorAll.bind(document)
+if (!id) var id = document.getElementById.bind(document)
+if (!createElement) var createElement = document.createElement.bind(document)
+
+const baseURL = 'https://webapps2.uc.edu/ecurriculum/DegreePrograms/Home/MajorMap/'
+const MAX_ITEMS = 10
+
+// TODO: Refactor this
+function submit() {
+    // Clear lists
+    Array.from($('.curriculum .list')).forEach(list => { list.innerText = '' })
+    /** @type {HTMLSelectElement} majors */
+    const majors = Array.from($('select')) || [], programStackList = []
+    for (const major of majors) programStackList.push(document.querySelector(`option[value="${major.value}"]`).id)
+    fetch(`/submit?q=${programStackList.join(',')}`, { method: 'POST' })
+        .then(res => res.json())
+        .then(data => {
+            // Handle UI updates (bring out to a global function?)
+            console.log(data)
+            // Assume that the order that we got the curriculums back is the same order that they are ordered on the page
+            const curriculumLists = Array.from($('.curriculum .list'))
+            for (let i = 0; i < curriculumLists.length; i++) {
+                /** @type {HTMLDivElement} curriculumListElement */
+                const curriculumListElement = curriculumLists[i],
+                /** @type {Array<string[]>} curriculumData */
+                curriculumData = data[i]
+                for (const course of curriculumData) {
+                    /** @type {HTMLParagraphElement} listItem */
+                    const listItem = createElement('p')
+                    listItem.className = 'list-item'
+                    listItem.innerText = course.join(' ')
+                    curriculumListElement.appendChild(listItem)
+                }
+            }
+        })
+}
+
+// TODO: Change this function to just simply add a new element instead of weirdly changing the innerHTML every time
+function addCurriculum() {
+    /** @type {HTMLElement} pageContent */
+    const pageContent = id('main')
+    if (pageContent.children.length < MAX_ITEMS) {
+        /** @type HTMLDivElement newCurriculum */
+        const newCurriculum = createElement('div')
+        newCurriculum.className = 'curriculum'
+        newCurriculum.innerHTML = `
+        <div class="selector">
+        <label for="major-select">Curriculum: </label>
+        <select name="major" id="major-select">
+            <option value="">-- Select Your Major --</option>
+        </select>
+        </div>
+        <div class="list"></div>
+        <button class="button remove" onclick="remove(this)">Remove</button>`
+        pageContent.appendChild(newCurriculum)
+    }
+}
+
+// Using this class to keep track of what sort of properties we are interested in, may also be useful if we move to a more OOP approach
+class Program {
+    constructor (props) {
+        /**
+         * @type {string | null} ProgramTitle (ex: Computer Science)
+         * @default null
+         */
+        this.ProgramTitle = props.ProgramTitle || null
+        /** 
+         * @type {string | null} DegreeCode (ex: BSCS)
+         * @default null
+         */
+        this.DegreeCode = props.DegreeCode || null
+        /**
+         * @type {string | null} LocationCode (ex: West Campus)
+         * @default null
+         */
+        this.LocationCode = props.LocationCode || null
+        /** @type {string | null} CollegeCode (ex: Engineering & Applied Science) @default null */
+        this.CollegeCode = props.CollegeCode || null
+        /** 
+         * @type {boolean} HasMajorMap
+         * @default false
+         */
+        this.HasMajorMap = props.HasMajorMap || false
+        /** @type {string | null} DisplayDegree */
+        this.DisplayDegree = props.DisplayDegree || null
+        /**
+         * @description  Ex: 20BC-ASE-BSAERO | Used to create the URL for the major map
+         * @type {string | null} ProgramStack
+         * @default null
+         */
+        this.ProgramStack = props.ProgramStack || null
+    }
+}
+
+function requestMajorList() {
+    // Default headers provided by the browsers should be fine for the time being
+    fetch('/major-info', { method: 'POST' })
+        .then(res => res.json())
+        .then((/** @type {Program[]} data*/ data) => {
+            // console.log(data)
+            data.forEach(program => {
+                /** @type {HTMLOptionElement} item */
+                const item = createElement('option')
+                item.id = program.ProgramStack
+                item.innerText = program.ProgramTitle
+                item.value = program.ProgramTitle
+                // The following line is temporary
+                $('select').forEach(elem => elem.appendChild(item.cloneNode(true)))
+            })
+        })
+}
+
+/**
+ * @param {HTMLButtonElement} element 
+ */
+function remove(element) {
+    element.parentElement.remove()
+}
+
+window.onload = () => {
+    console.log('Pinning for the fjords')
+    // This is for the prototype only, will need refactored/removed later
+    requestMajorList()
+}
---
+++ web/server.mjs
@@ -0,0 +1,80 @@
+import express from 'express'
+const app = express()
+app.use(express.static('./public'))
+
+import { createServer } from 'http'
+const server = createServer(app)
+
+import { scrape } from '../scraper.mjs'
+
+import * as programList from './program-list.json' assert { type: 'json' }
+
+// Using this class to keep track of what sort of properties we are interested in, may also be useful if we move to a more OOP approach
+class Program {
+    constructor (props) {
+        /** 
+         * @type {string | null} ProgramTitle (ex: Computer Science)
+         * @default null
+         */
+        this.ProgramTitle = props.ProgramTitle || null
+        /** 
+         * @type {string | null} DegreeCode (ex: BSCS)
+         * @default null
+         */
+        this.DegreeCode = props.DegreeCode || null
+        /** 
+         * @type {string | null} LocationCode (ex: West Campus)
+         * @default null
+         */
+        this.LocationCode = props.LocationCode || null
+        /**
+         * @type {string | null} CollegeCode (ex: Engineering & Applied Science)
+         * @default null
+         */
+        this.CollegeCode = props.CollegeCode || null
+        /**
+         * @type {boolean} HasMajorMap
+         * @default false
+         */
+        this.HasMajorMap = props.HasMajorMap || false
+        /** @type {string | null} DisplayDegree */
+        this.DisplayDegree = props.DisplayDegree || null
+        /**
+         * @description  Ex: 20BC-ASE-BSAERO | Used to create the URL for the major map
+         * @type {string | null} ProgramStack
+         * @default null
+         */
+        this.ProgramStack = props.ProgramStack || null
+    }
+}
+
+// 404 handeling
+app.get('*', (req, res) => {
+    console.log(req.path)
+    res.status(404).send('Invalid URL path')
+})
+
+// TODO: Refactor to allow for multiple scrapes
+app.post('/submit', async (req, res) => {
+    console.log(req.originalUrl)
+    console.log(req.query)
+    // req.query.q should be a comma seperated list of program stacks
+    // Still need to do some level of input validation
+    const r = await scrape(req.query['q'].split(','))
+    res.send(r)
+})
+
+app.post('/major-info', async (req, res) => {
+    console.log(req.originalUrl, '\n', req.params)
+    /** @type {Program[]} result */
+    const result = await programList.default.data.filter( /** @type {Program} program */ (program) => {
+        return program.HasMajorMap &&
+               program.CollegeCode === 'Engineering & Applied Science' &&
+               program.LocationCode === 'West Campus' && 
+               program.DegreeCode.startsWith('BS')
+    })
+    res.send(result)
+})
+
+// Start server on localhost/127.0.0.1 on port 8080
+server.listen(8080, () => console.info('Online at ', server.address()))

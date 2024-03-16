7db8345bb12e521afabb60ca5083396b94566f59
Colin Davis <colindavis5@gmail.com>
2024-03-16 15:32:45 -0400

    Update scraper to use program stacks and be promise based

--- scraper.mjs
+++ scraper.mjs
@@ -3,9 +3,9 @@ import axios from "axios"
 import * as cheerio from "cheerio"
 
 // TODO: Should probably try and clean this up a bit, feels messy
-const baseURL ="https://webapps2.uc.edu/ecurriculum/DegreePrograms/Home/MajorMap/",
+const baseURL ="https://webapps2.uc.edu/ecurriculum/degreeprograms/program/majormap/",
 courseCodesExp = /[A-Z]+\d+[A-Z]{0,1}/g,
-validClass = /^([A-Z]+\d+[A-Z]{0,1}).*(\d)$/,
+validClass = /^([A-Z]+\d+[A-Z]{0,1}) (.*) (\d)$/,
 blaskListCourseCodes = /(?:^COOP)/g,
 whitespace = /[\t\n\r]+/g,
 captionBlacklist = /COOP|Senior Design|\WSelect\W|Contemporary|Tra+ck|Elective/g
@@ -17,54 +17,60 @@ captionBlacklist = /COOP|Senior Design|\WSelect\W|Contemporary|Tra+ck|Elective/g
 */
 async function scrapeIndividual(curriculumStack, debug = false) {
     // TODO: Deal with validating that the given curriculum actually exists (use fetch and check status code in response headers?)
-    // NOTE: Maybe try chaining Promise#then calls after this get to get around the issue of function returning early?
-    const curriculum = []
-    const response = await axios.get(baseURL + curriculumStack)
-    // Cheerio will handle any html parsing, allowing us to use a JQuery like syntax to scan the DOM
-    const $ = cheerio.load(response.data)
-    // Grab all of the table elements on the page
-    const tables = $('table')
-    tables.each((i, element) => {
-        const tableId = $(element).attr().id,
-        // The caption element on each table holds the header, we can use this to throw out some groups of classes that we don't want to include
-        tableCaption = $(`#${tableId} caption`).text().trim().replace(/\t+|\n|\r/g, ' ').replace(/ {2,}/g, '')
-        // Check if the header is valid
-        if (!captionBlacklist.test(tableCaption)) {
-            console.log(tableCaption)
-            const rowData = []
-            // Grab all of the row data in the current class, each row in a table contains data for an individual class (some exceptions apply)
-            $(`#${tableId} tr`).each((i, row) => {
-                const classData = $(row).text().replace(whitespace, ' ').replace(/ {2,}/g, '').trim(),
-                courseCodes = classData.match(courseCodesExp) || []
-                // console.log(classData)
-                if (courseCodes.length === 1 && !blaskListCourseCodes.test(courseCodes[0]) && validClass.test(classData)) {
-                    // Extract various bits of good information
-                    const courseData = validClass.exec(classData), courseCode = courseData[1], credits = courseData[2]
-                    rowData.push(classData)
-                }
-            })
-            // console.log(rowData)
-            curriculum.push(...rowData)
+    // TODO: Clean this up, it needs it
+    return new Promise(async (resolve) => {
+        const curriculum = []
+        const response = await axios.get(baseURL + curriculumStack, { headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36' } })
+        if (debug) console.log('Axios done', response.status, response.statusText)
+        // Cheerio will handle any html parsing, allowing us to use a JQuery like syntax to scan the DOM
+        const $ = cheerio.load(response.data)
+        // Grab all of the table elements on the page
+        const tables = $('.table')
+        // console.log('Grabbed tables from cheerio parse', tables, tables.length)
+        // This may be able to get refactored later (read: should)
+        for (let i = 0; i < tables.length; i++) {   
+            const element = tables.get(i)
+            const tableId = $(element).attr().id,
+            // The caption element on each table holds the header, we can use this to throw out some groups of classes that we don't want to include
+            tableCaption = $(`#${tableId} caption`).text().trim().replace(/\t+|\n|\r/g, ' ').replace(/ {2,}/g, '')
+            // Check if the header is valid
+            if (!captionBlacklist.test(tableCaption)) {
+                // console.log(tableCaption)
+                const rowData = []
+                // Grab all of the row data in the current class, each row in a table contains data for an individual class (some exceptions apply)
+                $(`#${tableId} tr`).each((i, row) => {
+                    const classData = $(row).text().replace(whitespace, ' ').replace(/ {2,}/g, '').trim(),
+                    courseCodes = classData.match(courseCodesExp) || []
+                    // console.log(classData)
+                    if (courseCodes.length === 1 && !blaskListCourseCodes.test(courseCodes[0]) && validClass.test(classData)) {
+                        // Extract various bits of good information
+                        const courseData = validClass.exec(classData), courseCode = courseData[1], className = courseData[2], credits = courseData[3]
+                        // console.log(courseData, courseCode, className, credits)
+                        rowData.push([courseCode, className, credits])
+                    }
+                })
+                curriculum.push(...rowData)
+            }
+            if (debug) {
+                // Log results for debugging
+                console.log(curriculum)
+                console.log(curriculum.length)
+            }
+            if (i === tables.length - 1) resolve(curriculum)
         }
     })
-    if (debug) {
-        // Log results for debugging
-        console.log(curriculum)
-        console.log(curriculum.length)
-    }
-    // NOTE: This function is returning before the scrape is complete, fix this
-    return curriculum
 }
 
 /**
  * This should be the only function accessing scrapeIndividual
  * @param {Array<string> | null} curriculumStacks List of major identifying information used to create the urls to scape
  * @param {Boolean} debug Should the script output log statements?
- * @returns string[][]
+ * @returns {Promise<string[][]>}
  */
 export async function scrape(curriculumStacks = null, debug = false) {
     const result = []
     for (const stack of curriculumStacks) result.push(await scrapeIndividual(stack, debug))
+    console.log(result)
     return result
 }
 

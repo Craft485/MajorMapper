eb6a40ca29c9a3da67c89d52d1821d10c2f10cd0
Craft485 <colindavis5@gmail.com>
2024-08-31 16:12:29 -0400

    Update typings

--- src/types/backend.d.ts
+++ src/types/backend.d.ts
@@ -1,6 +1,10 @@
-import { Request, Response } from "express"
+import { Request, Response } from 'express'
+import { Program } from './university'
 
 export type EndPoint = {
     type: 'POST' | 'GET'
     call: (req: Request, res: Response) => Promise<void>
 }
+
+// TODO: Build this type out more
+export type ProgramList = Program & { }

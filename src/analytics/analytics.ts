b29a72150882c70cf70dd64ed7dec0266145d196
Colin Davis <colindavis5@gmail.com>
2024-12-25 11:19:26 -0500

    Add first implementation of backend metric calculations

--- src/analytics/analytics.ts
+++ src/analytics/analytics.ts
@@ -1,9 +1,9 @@
 import { GenerateHexCodeFromCourseCode } from '../utils/color'
 import * as Analytics from '../types/analytics'
 import { OptimizeCurriculum } from './optimize'
-//import { CalculateMetrics } from './metrics'
 import { MergeCurricula } from './merge'
-import { readFile } from 'fs/promises'
+import { readFile, writeFile } from 'fs/promises'
+import { CalculateMetrics } from './metrics'
 
 export async function ParseAnalytics(programStacks: string[]): Promise<Analytics.Curriculum> {
     console.log(`Client requests the following stacks: ${programStacks.join(', ')}`)
@@ -19,6 +19,8 @@ export async function ParseAnalytics(programStacks: string[]): Promise<Analytics
         for (const course of curricula[0].semesters.flat()) {
             course.color = GenerateHexCodeFromCourseCode(course.courseCode)
         }
+        await CalculateMetrics(curricula[0])
+        await writeFile('./buffer.json', JSON.stringify(curricula[0]), { encoding: 'utf-8' })    
         return curricula[0]
     }
     console.log('Multiple curricula found')
@@ -27,6 +29,7 @@ export async function ParseAnalytics(programStacks: string[]): Promise<Analytics
         course.color = GenerateHexCodeFromCourseCode(course.courseCode)
     }
     const optimizedDegreePlan = await OptimizeCurriculum(mergedCurricula)
-
+    await CalculateMetrics(optimizedDegreePlan)
+    await writeFile('./buffer.json', JSON.stringify(optimizedDegreePlan), { encoding: 'utf-8' })
     return optimizedDegreePlan
 }
--- src/analytics/metrics.ts
+++ src/analytics/metrics.ts
@@ -1,6 +1,81 @@
 import { Curriculum, Vertex, Edge, Metrics } from '../types/analytics'
+import { calculateCoursePath } from './utils'
 
-/** Metrics are based on https://curricularanalytics.org/help/metrics */
-export async function CalculateMetrics(curriculum: Curriculum): Promise<void> {
-    return
+/** Metrics are based on https://curricularanalytics.org/help/metrics and https://cran.r-project.org/web/packages/CurricularAnalytics/vignettes/CurricularAnalytics.html */
+export async function CalculateMetrics(curriculum: Curriculum): Promise<Curriculum> {
+    const courses: Vertex[] = curriculum.semesters.flat()
+    curriculum.structuralComplexity = 0
+    for (const vertex of courses) { // Loop over every vertex in the graph, caclulate the courses metrics and total them to find all the degree plans metrics at the same time
+        // Get all courses that relate to the current course of interest, excluding any that have a mutual co-req relationship with the current vertex
+        const subset = (await calculateCoursePath(vertex, curriculum)).filter(course => !(course.edges.includes(vertex.courseCode) && vertex.edges.includes(course.courseCode)))
+        // Build an array of all path permutations through the current vertex of interest
+        const paths = await BuildPathPermutations(subset)
+        const pathLengths = paths.map(path => path.length)
+        // DF will be the longest path present in the list of path permutations
+        const DelayFactor = Math.max(...pathLengths)
+        // Calculate BF by finding the number of all unique courses that occur after the current course
+        const BlockingFactor = paths.map(path => path.slice(path.findIndex(course => course.courseCode === vertex.courseCode) + 1)).flat().filter((v, i, a) => a.findIndex(c => c.courseCode === v.courseCode) === i).length
+        const Centrality = pathLengths.reduce((acc, curr) => acc + curr, 0)
+
+        vertex.metrics = {
+            delayFactor: DelayFactor,
+            blockingFactor: BlockingFactor,
+            centrality: Centrality,
+            structualComplexity: DelayFactor + BlockingFactor
+        }
+        curriculum.structuralComplexity += vertex.metrics.structualComplexity
+    }
+    return curriculum
+}
+
+export async function BuildPathPermutations(courses: Vertex[], permutations: Vertex[][] = []): Promise<Vertex[][]> {
+    if (permutations.length === 0) { // This call is the top of the recurse chain
+        const sourceNodes = courses.filter(course => courses.find(vertex => vertex.edges.includes(course.courseCode)) === undefined)
+        for (const node of sourceNodes) { // Recursively build paths stemming from each source node
+            permutations.push(...await BuildPathPermutations(courses, [[node]]))
+        }
+        // console.log(permutations.map(path => path.map(v => v.courseCode).join(' -> ')).join('\n') + '\n\n\n\n')
+        return permutations
+    } else {
+        const newPermutations: Vertex[][] = []
+        let sinkNodeCount = 0
+        for (const permutation of permutations) {
+            const lastNode = permutation.at(-1)
+            const nextNodes = courses.filter(course => lastNode.edges.includes(course.courseCode) && !course.edges.includes(lastNode.courseCode))
+            if (nextNodes.length === 0) { // If we have already finished building this path, just save it and move on
+                newPermutations.push(permutation)
+                sinkNodeCount++
+                continue
+            }
+            for (const newNode of nextNodes) {
+                newPermutations.push(permutation.concat(newNode))
+            }
+        }
+        // console.log(newPermutations.map(path => path.map(v => v.courseCode).join(' -> ')).join('\n') + '\n\n\n\n')
+        // If every path we have ends in a sink node, then we are done, otherwise we need to recurse
+        return sinkNodeCount === permutations.length ? permutations : await BuildPathPermutations(courses, newPermutations)
+    }
+}
+
+/** @deprecated */
+async function CalculateLongestPath(vertices : Vertex[], courses: Vertex[], lookingForward?: boolean, pathLength: number = 0): Promise<number> {
+    if (lookingForward === undefined) { // Top-level recurse call
+        const backwardsAdjancencies = courses.filter(course => course.edges.includes(vertices[0].courseCode))
+        const forwardsAdjancencies = courses.filter(course => vertices[0].edges.includes(course.courseCode))
+        const pathLengthBack = await CalculateLongestPath(backwardsAdjancencies, courses, false, 1)
+        const pathLengthForward = await CalculateLongestPath(forwardsAdjancencies, courses, true, 1)
+        return pathLengthBack + pathLengthForward + 1
+    } else {
+        const newAdjacencies: Vertex[] = []
+        for (const vertex of vertices) {
+            newAdjacencies.push(
+                ...courses.filter(v => 
+                    lookingForward 
+                    ? vertex.edges.includes(v.courseCode) && !v.edges.includes(vertex.courseCode) // Do not consider mutual co-req relationships when determining metrics
+                    : v.edges.includes(vertex.courseCode) && !vertex.edges.includes(v.courseCode)
+                ).filter(v2 => newAdjacencies.find(course => v2.courseCode === course.courseCode) === undefined)
+            )
+        }
+        return newAdjacencies.length ? await CalculateLongestPath(newAdjacencies, courses, lookingForward, ++pathLength) : pathLength
+    }
 }

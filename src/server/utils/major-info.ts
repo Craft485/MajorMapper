9c538dd2eba39e52a9fb4f10341873e7e8344e4f
Colin Davis <colindavis5@gmail.com>
2025-01-30 00:06:44 -0500

    Fix typo in DegreeCodeWhiteList regex

--- src/server/utils/major-info.ts
+++ src/server/utils/major-info.ts
@@ -4,7 +4,7 @@ import { Program } from '../../types/university'
 import { data } from './program-list.json'
 
 async function GetProgramList(req: Request, res: Response): Promise<void> {
-    const DegreeCodeWhiteList = /BSCS|BSCOMPE|BSEE/
+    const DegreeCodeWhiteList = /^(BSCS|BSCOMPE|BSEE)$/ // NOTE: This is temporary while I have to write out JSON files by hand
     console.log(req.originalUrl, req.query || '')
     const result: Program[] = data.filter(program => {
         // This is temporary, in the future we can check url params for filters
